5ab2674bceb01dd8a124b17d117073a969298ce2
Vincent Zvolsky <vincent.zvolsky@ufl.edu>
2026-05-09 14:37:23 -0400

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one = 1                     # init idx 1 and 2 to 1
+        two = 1
+
+        for i in range(n - 1):      # for each step:
+            temp = one                  # store one in temp var
+            one += two                  # update one to one + two
+            two = temp                  # update two to old one/temp
+        
+        return one                  # return one
