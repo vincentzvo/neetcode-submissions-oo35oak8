b2f9a0c73ff3f4971711c0a0af3812f7b1c4c441
Vincent Zvolsky <vincent.zvolsky@ufl.edu>
2026-05-14 11:36:09 -0400

    Add: climbing-stairs - submission-9

---
+++ Data Structures & Algorithms/climbing-stairs/submission-9.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        l = 1
+        r = 1
+
+        for i in range(n - 1):
+            temp = l
+            l += r
+            r = temp
+        return l
