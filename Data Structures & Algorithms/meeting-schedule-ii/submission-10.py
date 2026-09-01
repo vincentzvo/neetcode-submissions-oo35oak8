563b56105c1ed6056cf61fbb709b79d7fc2d8fb3
Vincent Zvolsky <vincent.zvolsky@ufl.edu>
2026-09-01 16:08:56 -0400

    Add: meeting-schedule-ii - submission-10

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-10.py
@@ -0,0 +1,27 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        start = sorted([i.start for i in intervals])
+        end = sorted([i.end for i in intervals])
+        print(start)
+        print(end)
+
+        s = e = count = res = 0
+
+        while s < len(start):
+            if start[s] < end[e]:
+                s += 1
+                count += 1
+            else:
+                e += 1
+                count -= 1
+            res = max(res, count)
+        
+        return res
