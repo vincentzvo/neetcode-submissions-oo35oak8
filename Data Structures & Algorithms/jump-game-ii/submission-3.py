426ed53b01f668952339a710f4cae8546535271b
Vincent Zvolsky <vincent.zvolsky@ufl.edu>
2026-09-10 16:52:12 -0400

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        count = 0
+        l = r = 0
+
+        while r < len(nums) - 1:
+            maxJump = 0
+            for i in range(l, r + 1):
+                if nums[i] + i >= maxJump:
+                    maxJump = nums[i] + i
+                    l = i + 1
+            r = maxJump
+            count += 1
+        return count
