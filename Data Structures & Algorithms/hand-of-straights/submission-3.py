77a76929061b83eed60f1265b98d934b3619c56f
Vincent Zvolsky <vincent.zvolsky@ufl.edu>
2026-09-14 15:07:44 -0400

    Add: hand-of-straights - submission-3

---
+++ Data Structures & Algorithms/hand-of-straights/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize:
+            return False
+
+        count = Counter(hand)
+        hand.sort()
+
+        for n in hand:
+            if count[n]:
+                for i in range(n, n + groupSize):
+                    if not count[i]:
+                        return False
+                    count[i] -= 1
+        return True
